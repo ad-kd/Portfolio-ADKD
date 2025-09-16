0486542d297300390a33ca2e384ab71d6627fa68
Adhithya K <12218adhithyaksnm@gmail.com>
2025-09-16 15:02:47 +0530

    commit-1

--- src/App.jsx
+++ src/App.jsx
@@ -33,18 +33,7 @@ const App = () => {
     document.querySelectorAll('.animate-on-scroll').forEach(elem => {
       observer.observe(elem);
     });
-
     
-
-    // Form submission
-    // const contactForm = document.querySelector('form');
-    // if (contactForm) {
-    //   contactForm.addEventListener('submit', (e) => {
-    //     e.preventDefault();
-    //     // alert('Thank you for your message! I will get back to you soon.');
-    //     contactForm.reset();
-    //   });
-    // }
   }, [])
 
   return (
